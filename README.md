c91a9582cebbd4ce8cb4fd22ae30384964155fda
大橋健 <ohashi@m1air.local>
2021-01-30 23:47:48 +0900

    iPad supported

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # liveCam
+
+Live Cam viewer using Pythonista.
+
+Prepeare URLs for live cam up to six.
+Set up base URL and each number inside the script.
--- liveCam.py
+++ liveCam.py
@@ -5,29 +5,31 @@ url_cno =	[2059, 2060, 2061, 2062, 2063, 2037, 2039, 2047, 2048]
 webviews = []
 initFlag = False
 	
-def update(myInterface):
+def onAction(myInterface):
 	global initFlag
 	if initFlag == False:
 		viewSelector = myInterface.superview
-		for no in range(9):
+		for no in range(6):
 			webview = viewSelector['webview'+str(no)]
 			webview.load_url(url_base+str(url_cno[no]))
 			webviews.append(webview)
 		initFlag = True
 	else:
-		for no in range(9):
+		for no in range(6):
 			webviews[no].reload()
 			
 v = ui.load_view()
-update(v['button1'])
+onAction(v['button1'])
 sc = ui.get_screen_size()
-if sc[0] >= 768.0 and sc[1] >= 768.0:
+print('{0},{1}'.format(sc[0],sc[1]))
+if min(sc) >= 768.0:
 	# iPad
-	v.frame = (0, 0, 414, 896)
-	v.present('sheet') # fullscreen
+	v.frame = (0, 0, 900, 900)
+	v.present('fullscreen') # fullscreen
 	print('iPad')
 else:
 	# iPhone
+	v.frame = (0, 0, 540, 540)
 	v.present('sheet', orientations=['portrait'])
 	print('iPhone')
 
